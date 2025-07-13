b5564a16c22ec3b482a7aa78f6a02c48ea3d3e31
rs-quyhp <quyhp@runsystem.net>
2025-07-13 14:57:52 +0700

    114. meal details page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,15 +1,47 @@
-import Link from "next/link";
+import { getMeal } from "@/lib/meals";
+import Image from "next/image";
+import { Suspense } from "react";
+import styles from "./page.module.css";
+
+const Details = async ({ slug }) => {
+  const meal = await getMeal(slug);
+  meal.instructions = meal?.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
+};
 
 const MealDetail = ({ params }) => {
   const { slug } = params;
 
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>{slug}</h1>
-      <p style={{ textAlign: "center" }}>
-        <Link href={".."}>Back</Link>
-      </p>
-    </main>
+    <Suspense
+      fallback={<p className={styles.loading}>Loading meal details...</p>}
+    >
+      <Details slug={slug} />
+    </Suspense>
   );
 };
 
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import getMeals from "@/lib/meals";
+import { getMeals } from "@/lib/meals";
 import { Suspense } from "react";
 import MealGrid from "../components/meals/meals-grid";
 import styles from "./page.module.css";
