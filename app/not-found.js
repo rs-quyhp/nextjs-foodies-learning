1fa7860a97aa124f162a7f69eb76efb37bc22d58
rs-quyhp <quyhp@runsystem.net>
2025-07-13 14:32:32 +0700

    113. not found page

---
+++ app/not-found.js
@@ -0,0 +1,13 @@
+const NotFoundPage = () => {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunately, we couldn't find the requested page or resource.</p>
+      <p>
+        You can try to find what you're looking for in the navigation above.
+      </p>
+    </main>
+  );
+};
+
+export default NotFoundPage;
