23cc07b2e929ef579f92f090c45fc370a57f4ac5
rs-quyhp <quyhp@runsystem.net>
2025-07-13 14:25:21 +0700

    112. handling error

---
+++ app/meals/error.js
@@ -0,0 +1,12 @@
+"use client";
+
+const ErrorMealsPage = () => {
+  return (
+    <main className="error">
+      <h1>Error loading meals</h1>
+      <p>Failed to load meals. Please try again later.</p>
+    </main>
+  );
+};
+
+export default ErrorMealsPage;
